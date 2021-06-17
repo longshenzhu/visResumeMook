ea79aaca8b2084f2400c5962778c0504f8dde6af
李小兵 <lixb@zqnb.com.cn>
2021-06-17 10:23:11 +0800

    截止Electron与React结合起来之前

---
+++ webpack/webpack.base.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    //resolve配置Webpack如何寻找模块所对应的文件
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],      //表示在导入语句中没带文件后缀时，Webpack 会自动带上后缀去尝试访问文件是否存在。
+        alias: {
+            '@src': path.join(__dirname, '../', 'app/renderer')   //别名处理
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|ts|tsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            }
+        ]
+    },
+    plugins: [new CleanWebpackPlugin()],
+}
